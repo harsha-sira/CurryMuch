a8e2736e22489c9bc6da08a3383c98bf0007202b
harsha-sira <harshasandamal100@gmail.com>
2016-01-14 15:40:37 +0530

    Dyanamic loading buttons page

---
+++ CanteenWPF/MainWindow.xaml.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CanteenWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        SerialPort serialPort1 = new SerialPort("COM18", 9600, Parity.None, 8, StopBits.One);
+        String[] btnNames = { "Rice", "VEG 1", "VEG 2", "VEG 3", "VEG 4", "VEG 5", "Fish", "Egg", "Chicken", "Fish", "Egg", "Chicken", "Coke", "Ginger beer", "Fruit salad", "Fruit juice" };
+        int[] priceItem = { 50, 30, 30, 30, 30, 30, 40, 50, 60, 40, 50, 60, 40, 40, 70, 70 };
+        int totalprice;
+        Boolean[] buttonArrayClick = new Boolean[16];
+
+        /*
+         *clear the array  
+         */
+        private void clearButtonArrayClick()
+        {
+            for (int i = 0; i < buttonArrayClick.Length; i++)
+            {
+                buttonArrayClick[i] = false;
+            }
+        }
+        
+        
+        public MainWindow()
+        {
+            
+            InitializeComponent();
+            
+            //serial port coonection
+            //serialPort1.PortName = "COM18";
+            //try
+            //{
+            //    serialPort1.Open();
+            //}
+            //catch
+            //{
+            //    Environment.Exit(0);
+            //}
+
+            //Thread mainThread = new Thread(new ThreadStart(checker));
+            //mainThread.Start();
+
+            for (int i = 0; i < 16; i++)
+            {
+                System.Windows.Controls.Button newBtn = new Button();
+                System.Windows.Controls.Button newBtn1 = new Button();
+
+                newBtn.Content = btnNames[i];
+                newBtn.Background = null;
+                newBtn.Name = "Button" +i.ToString();
+                newBtn.FontSize = 24;
+                newBtn.FontStyle = FontStyles.Normal;
+                newBtn.Width = 200;
+                newBtn.Tag = i ;
+                newBtn.Height =70;
+               // Margin = new Thickness(50);
+                newBtn.Margin = new Thickness(0, 20, 0, 0);
+                newBtn.Click += new RoutedEventHandler(newBtn_Click);
+
+                if (i < 1)
+                {
+                    panel1.Children.Add(newBtn);
+                }
+                else if (i < 6)
+                {
+                    panel2.Children.Add(newBtn);
+                }
+                else if (i < 9)
+                {
+                    panel3.Children.Add(newBtn);
+                    
+                }
+                else if (i < 12)
+                {
+                    panel4.Children.Add(newBtn);
+                }
+                else
+                {
+                    panel5.Children.Add(newBtn);
+                }
+                
+               
+                
+            }
+            clearButtonArrayClick();
+           // grid1.Visibility = Visibility.Hidden;
+            grid2.Visibility = Visibility.Visible;
+        }
+
+        private void newBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Button btn = sender as Button;
+            int i = (int)btn.Tag;
+            
+            ImageBrush brush = new ImageBrush();
+            BitmapImage bitmap = new BitmapImage();
+            bitmap.BeginInit();
+            bitmap.UriSource = new Uri("img/tick.png", UriKind.RelativeOrAbsolute);
+            bitmap.EndInit();
+            brush.ImageSource = bitmap;
+
+            if (!buttonArrayClick[i])
+            {
+                buttonArrayClick[i] = true;
+                totalprice += priceItem[i];
+                btn.Background = brush;
+            }
+            else
+            {
+                buttonArrayClick[i] = false;
+                totalprice -= priceItem[i];
+                btn.Background = null;
+            }
+
+            
+            
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Console.Write(totalprice);
+        }
+       
+
+       
+    }
+}
